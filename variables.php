1a1980662a9817eb8ab3f2fbd2fa1c54a0c05a26
Ajay-Sah18 <ajaysah1374@gmail.com>
2026-09-16 16:46:46 +0545

    Practice PHP conditional statement

--- variables.php
+++ variables.php
@@ -1,25 +1,33 @@
 <?php
 
-$name = "Ajay";
-$age = 15;
-$height = 5.2;
-$isStudent = false;
+// $name = "Ajay";
+// $age = 65;
+// $height = 5.2;
+// $isStudent = false;
 
 
-echo "My name is " . $name . PHP_EOL;
-echo "I am " . $age . " years old." . PHP_EOL;
-echo "My height is " . $height . "." . PHP_EOL;
-echo "This is a " . $isStudent . "." . PHP_EOL;
+// echo "My name is " . $name . PHP_EOL;
+// echo "I am " . $age . " years old." . PHP_EOL;
+// echo "My height is " . $height . "." . PHP_EOL;
+// echo "This is a " . $isStudent . "." . PHP_EOL;
 
-var_dump("$name");
-var_dump($age);
-var_dump($height);
-var_dump($isStudent);
+// var_dump("$name");
+// var_dump($age);
+// var_dump($height);
+// var_dump($isStudent);
 
-$isLoggedIn = False;
+// $isLoggedIn = False;
 
-if($age >= 18){
-    echo "You are adult.";
+$marks = 50;
+
+if($marks >= 90){
+    echo "Grade A";
+} elseif($marks >= 80) {
+    echo "Grade B";
+} elseif($marks >=70){
+    echo "Grade C";
+} elseif($marks >= 60) {
+    echo "Grade D";
 } else {
-    echo "You are not adult.";
+    echo "Fail";
 }
