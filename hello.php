0faae270390689ec8a330b333b48d9640e148ace
Ajay-Sah18 <ajaysah1374@gmail.com>
2026-09-16 14:10:33 +0545

    Add first PHP Hello program

---
+++ hello.php
@@ -0,0 +1,5 @@
+<?php
+echo "Hello, Ajay! Welcome to PHP." . PHP_EOL;
+echo "My name is Ajay." . PHP_EOL;
+echo "I am learning PHP." . PHP_EOL;
+echo "I want to become a Laravel Developer." . PHP_EOL;
