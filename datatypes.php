9c79ac0a2a0028168231b7589af9652d3e4a7ab9
Ajay-Sah18 <ajaysah1374@gmail.com>
2026-09-17 09:12:11 +0545

    Learn PHP data types

---
+++ datatypes.php
@@ -0,0 +1,23 @@
+<?php
+
+// $name = "Ajay";
+// $age = 23;
+// $height = 5.8;
+// $city = "Biratnagar";
+// $students = 50;
+// $price = 99.99;
+
+// var_dump($name);
+// var_dump($age);
+// var_dump($height);
+// var_dump($city);
+// var_dump($students);
+// var_dump($price);
+
+$isStudent = true;
+$isWorking = false;
+
+var_dump($isStudent);
+var_dump($isWorking);
+
+?>
