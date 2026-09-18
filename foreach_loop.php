a1be356ad8d0f0219467320afe5cb7182ae0fdbd
Ajay-Sah18 <ajaysah1374@gmail.com>
2026-09-18 19:07:12 +0545

    Learn PHP foreach loop

---
+++ foreach_loop.php
@@ -0,0 +1,40 @@
+<?php
+
+// $skills = ["PHP", "Laravel", "Git", "Linux", "SQL"];
+
+// foreach ($skills as $skill){
+//     echo "I am learning " . $skill . "." . PHP_EOL;
+    
+// }
+
+// $students = [
+//     "name" => "Ajay",
+//     "age" => 23,
+//     "goal" => "Laravel Developer"
+// ];
+
+// foreach ($students as $key => $value) {
+//  echo $key . ":" . $value . PHP_EOL;
+// }
+
+//  $students = [
+//     "name" => "Ajay",
+//      "age" => 23,
+//      "city" => "Biratnagar",
+//     "goal" => "Laravel Developer"
+//  ];
+
+//  foreach ($students as $key => $value) {
+//   echo "My " . $key . " is: "  . $value . PHP_EOL;
+//  }
+
+ $course = [
+    "name" => "PHP",
+     "level" => "Beginner",
+     "duration" => "2 Months",
+    "goal" => "Laravel Developer"
+ ];
+
+ foreach ($course as $key => $value) {
+  echo $key . ": "  . $value . PHP_EOL;
+ }
---
+++ skills.php
@@ -0,0 +1,7 @@
+<?php
+
+$skills = ["PHP", "Laravel", "Git", "Linux", "SQL"];
+
+foreach ($skills as $skill){
+    echo "I am learning " . $skill . PHP_EOL;
+}
