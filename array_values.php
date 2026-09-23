f2b24a7d6be4b17717df241298b586b37862039b
Ajay-Sah18 <ajaysah1374@gmail.com>
2026-09-23 14:43:43 +0545

    Learn PHP array_values function

---
+++ array_values.php
@@ -0,0 +1,16 @@
+<?php
+
+$students = [
+    101 => "Ajay",
+    105 => "suman",
+    110 => "Nitesh",
+    115 => "Abishek",
+    120 => "Roshan",
+    125 => "Niraj"
+];
+
+print_r(array_values($students));
+
+$studentNames = ["Prinka", "Sunita", "Anjana", "Aarti"];
+
+print_r(array_values($studentNames));
