054c4483123e64c94d65309967d7efab085d2f99
Ajay-Sah18 <ajaysah1374@gmail.com>
2026-09-25 23:11:20 +0545

    Learn PHP rsort function

--- sort.php
+++ sort.php
@@ -23,3 +23,29 @@ $ages = [24, 19, 32, 27, 21, 30];
 sort($ages);
 
 print_r($ages);
+
+//rsort() arrange array values from largest to smallest.
+
+//Practice 1
+
+$numbers = [25, 80, 45, 10, 60];
+
+rsort($numbers);
+
+print_r($numbers);
+
+//Practice 2
+
+$salaries = [35000, 50000, 28000, 45000, 60000];
+
+rsort($salaries);
+
+print_r($salaries);
+
+//Practice 3
+
+$ages = [22, 31, 19, 28, 25, 35];
+
+rsort($ages);
+
+print_r($ages);
